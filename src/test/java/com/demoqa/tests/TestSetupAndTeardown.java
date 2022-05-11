fa5f215e7665d95208818fc02ca038ab779be172
Alisa.Kadilenko <alisa.kadilenko@nexign.com>
2022-05-11 08:17:31 +0300

    Modify Gradle Build

--- src/test/java/com/demoqa/tests/TestSetupAndTeardown.java
+++ src/test/java/com/demoqa/tests/TestSetupAndTeardown.java
@@ -22,7 +22,7 @@ public class TestSetupAndTeardown {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        open("https://demoqa.com");
+//        open("https://demoqa.com");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
